59a3818cce1a4b5edb08598c70efbc4a9c04097e
zanisis <adzannyaa@gmail.com>
2017-04-25 12:03:23 +0700

    REST API Basic with Postman

---
+++ seeders/20170425040622-users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      name: 'John Doe',
+      email: 'john@gmail.com'
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
