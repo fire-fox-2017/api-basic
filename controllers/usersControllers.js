35c315ccb2247be18038307956ba619a14ede33c
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-25 11:47:18 +0700

    add getall,getone and create

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -2,7 +2,28 @@ var db = require('../models');
 module.exports = {
   getall: (req, res, next) => {
     db.User.findAll().then(ArrResult => {
-      res.send(ArrResult)
+      res.send(ArrResult);
+    })
+  },
+  getone: (req, res, next) => {
+    let id = req.params.id;
+    db.User.findById(id).then(Result => {
+      res.send(Result);
+    })
+  },
+  create: (req, res, next) => {
+    let name = req.body.name;
+    let age = req.body.age;
+    let email = req.body.email;
+    console.log(`name ${name} age ${age} email ${email}`);
+    db.User.create({
+      name: name,
+      age: age,
+      email: email
+    }).then(Result => {
+      res.send(Result);
+    }).catch(err => {
+      res.send(err);
     })
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var controllers = require('../controllers/usersControllers');
 
 router.get('/', controllers.getall);
+router.get('/:id', controllers.getone);
+router.post('/', controllers.create);
 
 module.exports = router;
